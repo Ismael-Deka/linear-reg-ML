12f4248582625177c7c59b0cddb4d1a8dc1f2e74
Ismael-Deka <ismael.deka7899@gmail.com>
2023-04-10 17:40:40 -0400

    added momentum coefficient

--- train.py
+++ train.py
@@ -61,6 +61,16 @@ learn_rate =  2
 lr_coff =     0.01
 cost_history = []
 cost_history.append(cost)
+momentum = 0.9
+
+grad_one = 0
+grad_two = 0
+grad_three = 0
+grad_4 = 0
+grad_5 = 0
+
+
+
 while(cost >= 1000):
     W_p_1 = W_1
     W_p_2 = W_2
@@ -69,11 +79,11 @@ while(cost >= 1000):
     W_p_5 = W_5
     b_p =b
     y_pred_p = y_pred
-    grad_one = gradient(X_1_train, y_train, y_pred)
-    grad_two = gradient_deriv(X_1_train, X_2_train, y_train, y_pred)
-    grad_three = gradient_deriv(X_1_train, X_3_train, y_train, y_pred)
-    grad_4 = gradient_deriv(X_1_train, X_4_train, y_train, y_pred)
-    grad_5 = gradient_deriv(X_1_train, X_5_train, y_train, y_pred)
+    grad_one = gradient(X_1_train, y_train, y_pred) + (grad_one*momentum)
+    grad_two = gradient_deriv(X_1_train, X_2_train, y_train, y_pred)  + (grad_two*momentum)
+    grad_three = gradient_deriv(X_1_train, X_3_train, y_train, y_pred) + (grad_three*momentum)
+    grad_4 = gradient_deriv(X_1_train, X_4_train, y_train, y_pred) + (grad_4*momentum)
+    grad_5 = gradient_deriv(X_1_train, X_5_train, y_train, y_pred) + (grad_5*momentum)
     if(is_algo_stuck(cost_history)):
         break;
     
@@ -102,7 +112,7 @@ while(cost >= 1000):
         
         #print("learn rate updated to %.20f" % learn_rate)
         continue
-    abs(learn_rate)
+    learn_rate = abs(learn_rate)
     print("Iteration: %d" % i)
     print("Learn rate: %.90f" % learn_rate)
     print("Cost: %f" % cost)
